40bf93f314b71b5ad26b739f93d716c4a7b868ff
walczuk135 <walczuk135@gmail.com>
2018-02-26 18:46:12 +0100

    Nowa zmiana

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("witaj świecie");
+        System.out.println("witaj świecie !!!!");
     }
 }
